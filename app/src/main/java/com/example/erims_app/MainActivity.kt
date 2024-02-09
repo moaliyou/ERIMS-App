e0284e77245cfd6d44486d9fdc5c6e3c4b1c8e31
moaliyou <moaliyou47@gmail.com>
2024-02-09 19:51:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/erims_app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.erims_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.erims_app.ui.theme.ERIMSAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ERIMSAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
