9fca4e070ffd6cf01fb616e040b6d93009e4d08d
moaliyou <moaliyou47@gmail.com>
2024-02-13 22:39:30 +0300

    refactor: make the date picker state less/hoist it

--- app/src/main/java/com/example/erims_app/MainActivity.kt
+++ app/src/main/java/com/example/erims_app/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.erims_app.ui.components.CustomDatePicker
 import com.example.erims_app.ui.theme.ERIMSAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val datePickerState = rememberDatePickerState()
+                    CustomDatePicker(datePickerState = datePickerState)
                 }
             }
         }
--- app/src/main/java/com/example/erims_app/ui/screens/employee/EmployeeEntryScreen.kt
+++ app/src/main/java/com/example/erims_app/ui/screens/employee/EmployeeEntryScreen.kt
@@ -1,41 +1,23 @@
 package com.example.erims_app.ui.screens.employee
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.erims_app.R
 import com.example.erims_app.ui.theme.ERIMSAppTheme
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 @Composable
 fun EmployeeEntryScreen() {
@@ -109,94 +91,7 @@ private fun EmployeeForm(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth()
         )
         val datePickerState = rememberDatePickerState()
-        TransactionDatePicker(datePickerState = datePickerState)
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TransactionDatePicker(datePickerState: DatePickerState) {
-    Box(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.surfaceVariant)
-            .padding(16.dp, 8.dp)
-    ) {
-        val showDialog = remember { mutableStateOf(false) }
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    showDialog.value = true
-                },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                modifier = Modifier
-                    .weight(1f),
-                text = if (datePickerState.selectedDateMillis != null) {
-                    SimpleDateFormat(
-                        "MMMM d, yyyy",
-                        Locale.getDefault()
-                    ).format(datePickerState.selectedDateMillis)
-                } else {
-                    "Choose Date"
-                },
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
-            )
-            Icon(
-                imageVector = Icons.Default.DateRange,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                modifier = Modifier.padding(8.dp)
-            )
-        }
-
-
-        if (showDialog.value) {
-            val confirmEnabled =
-                remember { derivedStateOf { datePickerState.selectedDateMillis != null } }
-            DatePickerDialog(
-                onDismissRequest = {
-                    showDialog.value = false
-                },
-                confirmButton = {
-                    TextButton(
-                        onClick = {
-                            showDialog.value = false
-
-                        },
-                        enabled = confirmEnabled.value
-                    )
-                    {
-                        Text("Ok")
-                    }
-                },
-                dismissButton = {
-                    TextButton(
-                        onClick = {
-                            showDialog.value = false
-                        }
-                    ) {
-                        Text("Dismiss")
-                    }
-                }
-            ) {
-                DatePicker(state = datePickerState, showModeToggle = false)
-            }
-        }
-
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
-@Composable
-fun DatePickerPreview() {
-    val datePickerState = rememberDatePickerState()
-    ERIMSAppTheme {
-        TransactionDatePicker(datePickerState = datePickerState)
+        //TransactionDatePicker(datePickerState = datePickerState)
     }
 }
 
