e596e440bdaaf9d224076016642f02406e60860e
kuredema7 <kuredema7@gmail.com>
2024-02-15 11:52:31 +0300

    fix: added text styling size for the summary report card

--- app/src/main/java/com/example/erims_app/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/erims_app/ui/screens/home/HomeScreen.kt
@@ -1,17 +1,24 @@
 package com.example.erims_app.ui.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,27 +38,39 @@ fun HomeDashboard() {
 @Composable
 fun HomeContent(modifier: Modifier = Modifier) {
     Card(modifier = modifier) {
-        Column(
+        Row(
             modifier = Modifier
                 .padding(dimensionResource(R.dimen.extra_medium_padding))
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.medium_padding))
         ) {
-            Row(
+            Box (
+                modifier = Modifier
+                    .size(dimensionResource(R.dimen.icon_container_size))
+                    .padding(dimensionResource(R.dimen.medium_padding))
+                    .clip(RoundedCornerShape(dimensionResource(R.dimen.medium_padding)))
+                    .background(MaterialTheme.colorScheme.tertiaryContainer),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    Icons.Outlined.Person,
+                    contentDescription = null,
+                    modifier = Modifier.size(dimensionResource(R.dimen.icon_size))
+                )
+            }
+            Column(
                 modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
             ) {
                 Text(
                     text = stringResource(R.string.employee_card_label),
-                    modifier = Modifier.weight(1f)
+                    style = MaterialTheme.typography.bodyLarge
                 )
-                Icon(
-                    Icons.Outlined.Person,
-                    contentDescription = null
+                Text(
+                    text = "569",
+                    style = MaterialTheme.typography.titleLarge
                 )
             }
-            Text(
-                text = "569"
-            )
         }
     }
 }
