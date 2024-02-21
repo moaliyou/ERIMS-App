d09384ddc7a4cbcfcc59cb58fee22e7e21ac4a6f
kuredema7 <kuredema7@gmail.com>
2024-02-21 17:12:52 +0300

    fix: add bottom navigation for screen navigation

---
+++ app/src/main/java/com/example/erims_app/ui/screens/main/MainScreen.kt
@@ -0,0 +1,51 @@
+package com.example.erims_app.ui.screens.main
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.erims_app.ui.components.CustomNavigationBar
+import com.example.erims_app.ui.navigation.AppNavHost
+import com.example.erims_app.ui.navigation.Screen
+
+@Composable
+fun MainScreen() {
+    val navController: NavHostController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    Scaffold(
+        bottomBar = {
+            CustomNavigationBar(
+                navigationItemList = Screen.getNavigationItems(),
+                destination = currentDestination,
+                onNavigationItemSelected = { selectedItem ->
+                    navController.navigate(selectedItem) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // re-selecting the same item
+                        launchSingleTop = true
+                        // Restore state when re-selecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    ) {
+        AppNavHost(
+            navController = navController,
+            modifier = Modifier.padding(it)
+        )
+    }
+
+}
