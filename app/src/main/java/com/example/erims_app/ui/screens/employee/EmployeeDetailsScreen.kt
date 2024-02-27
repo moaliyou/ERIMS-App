6ed788e7a7aa0114291de73e905d08997aceb48e
kuredema7 <kuredema7@gmail.com>
2024-02-27 11:58:31 +0300

    fix: add bottom navigation for screen navigation

--- app/src/main/java/com/example/erims_app/ui/screens/employee/EmployeeDetailsScreen.kt
+++ app/src/main/java/com/example/erims_app/ui/screens/employee/EmployeeDetailsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
