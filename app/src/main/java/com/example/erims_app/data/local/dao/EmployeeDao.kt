8a0a53090b9186d196b953197ac92b219be98583
moaliyou <moaliyou47@gmail.com>
2024-03-17 20:35:29 +0300

    fix: add employee data access object for CRUD operation

---
+++ app/src/main/java/com/example/erims_app/data/local/dao/EmployeeDao.kt
@@ -0,0 +1,26 @@
+package com.example.erims_app.data.local.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.erims_app.data.local.entities.Employee
+import kotlinx.coroutines.flow.Flow
+
+interface EmployeeDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(employee: Employee)
+
+    @Update
+    suspend fun update(employee: Employee)
+
+    @Delete
+    suspend fun delete(employee: Employee)
+
+    @Query("SELECT * FROM employees WHERE id = :id")
+    fun getEmployee(id: Int): Flow<Employee>
+
+    @Query("SELECT * FROM employees ORDER BY firstName ASC")
+    fun getAllEmployees(): Flow<List<Employee>>
+}
