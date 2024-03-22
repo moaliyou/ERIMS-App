bed74870d9bc7c958219d735b9392fc798a8b1ea
moaliyou <moaliyou47@gmail.com>
2024-03-22 14:57:58 +0300

    fix: add swipe to delete card items

---
+++ app/src/main/java/com/example/erims_app/ui/components/SwipeToDelete.kt
@@ -0,0 +1,13 @@
+package com.example.erims_app.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+//import androidx.compose.material3.DismissState
+
+@Composable
+private fun DeleteBackground(
+    modifier: Modifier = Modifier,
+    //swipeDismissState: DismissState
+) {
+
+}
