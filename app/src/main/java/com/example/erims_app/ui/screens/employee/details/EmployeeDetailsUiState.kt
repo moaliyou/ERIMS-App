81cb531018da1bf29c87d16002dc38481c865cd2
kuredema7 <kuredema7@gmail.com>
2024-05-09 16:25:31 +0300

    refactor: rename the object datatype from entity class to the model class

--- app/src/main/java/com/example/erims_app/ui/screens/employee/details/EmployeeDetailsUiState.kt
+++ app/src/main/java/com/example/erims_app/ui/screens/employee/details/EmployeeDetailsUiState.kt
@@ -1,5 +1,5 @@
 package com.example.erims_app.ui.screens.employee.details
 
-import com.example.erims_app.data.local.entity.EmployeeEntity
+import com.example.erims_app.domain.model.Employee
 
-data class EmployeeDetailsUiState(val employeeList: List<EmployeeEntity> = listOf())
+data class EmployeeDetailsUiState(val employeeList: List<Employee> = listOf())
