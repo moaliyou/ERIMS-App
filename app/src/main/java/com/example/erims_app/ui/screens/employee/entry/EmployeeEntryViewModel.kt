ed7923105c07765d699ade7197a1370a6ad8a6b1
kuredema7 <kuredema7@gmail.com>
2024-05-09 16:28:01 +0300

    refactor: insert new line for readability

--- app/src/main/java/com/example/erims_app/ui/screens/employee/entry/EmployeeEntryViewModel.kt
+++ app/src/main/java/com/example/erims_app/ui/screens/employee/entry/EmployeeEntryViewModel.kt
@@ -2,18 +2,17 @@ package com.example.erims_app.ui.screens.employee.entry
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.erims_app.data.local.entity.EmployeeEntity
 import com.example.erims_app.domain.model.Employee
 import com.example.erims_app.domain.usecase.EmployeeUseCases
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.text.NumberFormat
 
 class EmployeeEntryViewModel(
     private val employeeUseCases: EmployeeUseCases
 ) : ViewModel() {
+
     private val _uiState = MutableStateFlow(EmployeeUiState())
     val employeeUiState = _uiState.asStateFlow()
 
@@ -33,5 +32,3 @@ class EmployeeEntryViewModel(
     }
 
 }
-
-fun EmployeeEntity.formattedSalary(): String = NumberFormat.getCurrencyInstance().format(salary)
